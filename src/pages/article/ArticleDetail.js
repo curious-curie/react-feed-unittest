03b2d14cb5075f62777440f49678816d2ced16a5
curious-curie <curieyoo@snu.ac.kr>
2020-09-29 20:38:11 +0900

    feat: detail & delete article

--- src/pages/article/ArticleDetail.js
+++ src/pages/article/ArticleDetail.js
@@ -2,19 +2,25 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import ArticleItem from '../../components/article/ArticleItem';
-import { getArticle } from '../../modules/articles';
+import { deleteArticle, getArticle } from '../../modules/articles';
 import history from '../../history';
 
 function ArticleDetail({ match }) {
   const dispatch = useDispatch();
   const { article } = useSelector((state) => state.article);
+  const { id } = match.params;
+
   useEffect(() => {
-    const { id } = match.params;
     dispatch(getArticle(id));
-  }, [dispatch, match.params]);
+  }, [dispatch, id]);
 
   const goBack = () => {
     history.push('/articles');
+  };
+
+  const onDelete = async () => {
+    await dispatch(deleteArticle(id));
+    goBack();
   };
 
   return (
@@ -22,7 +28,7 @@ function ArticleDetail({ match }) {
       <button id="back-detail-article-button" onClick={goBack}>
         ← back to list
       </button>
-      {article && <ArticleItem article={article} />}
+      {article && <ArticleItem article={article} onDelete={onDelete} />}
     </div>
   );
 }
