cbd8d8e1a5f078dcc2786f5352e8726a136bc230
curious-curie <curieyoo@snu.ac.kr>
2020-10-11 00:31:12 +0900

    app-test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { mount } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
 import App from './App';
+import { mockStore } from './mockStore';
+import rootReducer from './modules';
+import history from './history';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+describe('<Component /> unit test', () => {
+  const store = createStore(rootReducer, mockStore, composeWithDevTools(applyMiddleware(thunk)));
+
+  const getWrapper = () =>
+    mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <App />
+        </Router>
+      </Provider>,
+    );
+
+  it('App should mount', () => {
+    const wrapper = getWrapper();
+    expect(wrapper.find('.App').length).toBe(1);
+  });
 });
---
+++ src/mockStore.js
@@ -0,0 +1,5 @@
+export const mockStore = {
+  auth: { loading: false, error: false, user: null, users: [] },
+  article: { error: false, article: null, articles: [] },
+  comments: { error: false, comments: [] },
+};
